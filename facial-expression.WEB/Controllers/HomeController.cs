71ba74c109b39ac420f319b26b219df6b4d984dd
eduarchilon <eduarchilon@outlook.es>
2023-05-25 21:27:46 -0300

    feat(facial expression): create project in ML

---
+++ facial-expression.WEB/Controllers/HomeController.cs
@@ -0,0 +1,83 @@
+﻿using facial_expression.WEB.Models;
+using Facial_expression_WEB;
+using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.Extensions.Hosting.Internal;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace facial_expression.WEB.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+        public IActionResult procesarImagen()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult procesarImagen(Image model)
+        {
+            if (model.ImageFile != null && model.ImageFile.Length > 0)
+            {
+                // Aquí puedes realizar las operaciones necesarias con la imagen, como guardarla en disco, procesarla, etc.
+                // model.ImageFile contiene la imagen enviada
+
+                // Ejemplo: Guardar la imagen en una ubicación específica
+                var imagePath = $"images/{model.ImageFile.Name}.jpg";
+                using (var stream = new FileStream(imagePath, FileMode.Create))
+                {
+                    model.ImageFile.CopyTo(stream);
+                }
+
+
+                //Load sample data
+                var imageBytes = System.IO.File.ReadAllBytes(@$"images/{model.ImageFile}.jpg");
+                MLModel.ModelInput sampleData = new MLModel.ModelInput()
+                {
+                    ImageSource = imageBytes,
+                };
+
+                //Load model and predict output
+                var result = MLModel.Predict(sampleData);
+
+
+                Console.WriteLine("Respuesta:" + result.PredictedLabel);
+
+                // Ejemplo: Redirigir a una vista de éxito
+                return RedirectToAction("Index");
+            }
+
+            // Si no se envió una imagen válida, puedes redirigir a una vista de error o mostrar un mensaje de validación en el formulario.
+            ModelState.AddModelError("ImageFile", "Por favor, seleccione una imagen.");
+
+            // Vuelve a mostrar el formulario de carga de imágenes con el mensaje de error
+            return View("Index", model);
+        }
+    }
+}
---
+++ facial-expression.WEB/Models/Image.cs
@@ -0,0 +1,7 @@
+﻿namespace facial_expression.WEB.Models
+{
+    public class Image
+    {
+        public IFormFile ImageFile { get; set; }
+    }
+}
