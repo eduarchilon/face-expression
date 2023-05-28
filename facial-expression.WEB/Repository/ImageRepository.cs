2c42464b755b2e0e9075508891d3e273851fe961
eduarchilon <eduarchilon@outlook.es>
2023-05-28 15:48:51 -0300

    feat(index): change view in index

---
+++ facial-expression.WEB/Repository/ImageRepository.cs
@@ -0,0 +1,29 @@
+﻿using facial_expression.WEB.Models;
+
+namespace facial_expression.WEB.Repository
+{
+    public interface IImageRespository
+    {
+        List<Image> GetAllImages();
+    }
+
+    public class ImageRepository : IImageRespository
+    {
+
+        private List<Image> _images;
+
+        public ImageRepository(List<Image> images) { 
+            this._images = images;
+        }
+
+        public void Init()
+        {
+            this._images.Clear();
+        }
+
+        public List<Image> GetAllImages()
+        {
+            return null;
+        }
+    }
+}
